7e319658895762747aa5eb646578523d1637aeb0
Kristoffer-Aron <krh004@edu.zealand.dk>
2024-09-23 12:51:31 +0200

    Add Dog Object

--- emptyblanket/Program.cs
+++ emptyblanket/Program.cs
@@ -1,7 +1,15 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using EmptyBlanket;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("What the fork!");
 
 Console.WriteLine("O");
+
+Dog dog1 = new Dog(4, 6, 2);
+
+dog1.CommitBark();
+dog1.CommitDrool();
+dog1.CommitScream();
