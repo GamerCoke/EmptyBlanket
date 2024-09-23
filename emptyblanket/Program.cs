ead388972db3b0e10f5b1971e06980ab17cfcd7e
Mikkel Barfod <mib010@edu.zealand.dk>
2024-09-23 12:57:02 +0200

    add Katte object

--- emptyblanket/Program.cs
+++ emptyblanket/Program.cs
@@ -13,3 +13,7 @@ Dog dog1 = new Dog(4, 6, 2);
 dog1.CommitBark();
 dog1.CommitDrool();
 dog1.CommitScream();
+
+Katte kat1 = new Katte("Kitto Nitto Pepito Monfito, the Cheese Connesuer", 1989, "stribet", "sort & hvid");
+
+kat1.GetInfo();
