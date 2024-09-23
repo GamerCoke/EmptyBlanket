069b917b53bbf6c058f14d6d9b9eda2634d30f9f
Mikkel Barfod <mib010@edu.zealand.dk>
2024-09-23 13:09:37 +0200

    add write outs

--- emptyblanket/Program.cs
+++ emptyblanket/Program.cs
@@ -10,10 +10,10 @@ Console.WriteLine("O");
 
 Dog dog1 = new Dog(4, 6, 2);
 
-dog1.CommitBark();
-dog1.CommitDrool();
-dog1.CommitScream();
+Console.WriteLine($"{dog1.CommitBark()}");
+Console.WriteLine($"{dog1.CommitDrool()}");
+Console.WriteLine($"{dog1.CommitScream()}");
 
 Katte kat1 = new Katte("Kitto Nitto Pepito Monfito, the Cheese Connesuer", 1989, "stribet", "sort & hvid");
 
-kat1.GetInfo();
+Console.WriteLine($"{kat1.GetInfo()}");
