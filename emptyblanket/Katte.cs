582d61be8796c2426dd7cc5830710d546eda6c20
Mikkel Barfod <mib010@edu.zealand.dk>
2024-09-23 12:43:16 +0200

    add Katte Class add basic Katte function

---
+++ emptyblanket/Katte.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyBlanket
+{
+    internal class Katte
+    {
+
+        public Katte (string navn, int føsår, string mønster, string farve)
+        {
+            Name = navn;
+            YoB = føsår;
+            Pattern = mønster;
+            Color = farve;
+            
+        }
+        public string Name { get; set; }
+        public int YoB { get; set; }
+        public string Pattern { get; set; }
+        public string Color { get; set; }
+
+        public string GetInfo()
+        {
+            return $"{Name} was born {YoB}, is {Color} and has a {Pattern} pattern.";
+        }
+
+        
+    }
+}
